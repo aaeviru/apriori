25ca8178db16593ec14d2543003aedb1f0d48f51
aaeviru <aaeviru@gmail.com>
2017-02-14 03:38:09 +0000

    final

--- apriori.cpp
+++ apriori.cpp
@@ -1,7 +1,11 @@
 #include "apriori.h"
 
 int main(int argc,char** argv){
-  if(argc < 4) return -1;
+  if(argc != 4){
+     printf("input:input-filename output-filename min-sup\n");
+     return -1;
+  }
+  printf("min-sup:%s\n",argv[3]);
   Tries test;
   test.setifof(argv[1],argv[2]);
   test.setms(atoi(argv[3]));
